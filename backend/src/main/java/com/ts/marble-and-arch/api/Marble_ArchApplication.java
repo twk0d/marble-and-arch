5972e221651ca85295572bc9ef2d5896c4d2fe6c
twk0d <twkod@pm.me>
2026-04-20 11:00:00 -0300

    chore(backend): initial spring boot setup and gradle config

---
+++ backend/src/main/java/com/ts/marble-and-arch/api/Marble_ArchApplication.java
@@ -0,0 +1,13 @@
+package com.ts.marblearch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarbleArchApplication.class, args);
+	}
+
+}
